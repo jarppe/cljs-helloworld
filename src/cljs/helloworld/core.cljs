52b9b55674931be45a83d100b77a8c09c3d85f2d
jarppe <jarppe@metosin.fi>
2012-09-01 14:22:00 +0300

    Removed compiled JS, trimmed for clarity

--- src/cljs/helloworld/core.cljs
+++
@@ -1,7 +0,0 @@
-(ns helloworld.core
-  (:use [clojure.string :only [split-lines]])
-  (:require [helloworld.graph :as g]
-            [helloworld.canvas :as c]))
-
-(defn ^:export main []
-  (g/draw-text (c/get-context) 150 150 (split-lines "hello\nworld")))
