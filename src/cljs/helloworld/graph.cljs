fee3e120f108c4ca63fba1dcd84e41bf2d45644e
jarppe <jarppe@metosin.fi>
2012-09-01 19:02:19 +0300

    Refactored and changed appearance to Hercules monitor.

--- src/cljs/helloworld/graph.cljs
+++ src/cljs/helloworld/graph.cljs
@@ -5,16 +5,16 @@
 
 (defn make-fill [context]
   (doto (.createRadialGradient context 0 0 0 0 0 17)
-    (.addColorStop 0 "rgba(0,255,0,255)")
-    (.addColorStop 1 "rgba(0,255,0,0)")))
+    (.addColorStop 0 "rgba(255,255,0,255)")
+    (.addColorStop 1 "rgba(255,255,0,0)")))
 
 (defn draw-figure [context fx fy pixels]
   (dorun
     (for [y (range 8) x (range 5) :when (= (get pixels (+ x (* y 5))) 1)]
-      (do
-        (.setTransform context 1 0 0 1 (+ fx (* x 20)) (+ fy (* y 20)))
-        (c/set-fill! context (make-fill context))
-        (.fillRect context -20 -20 40 40)))))
+      (doto context
+        (.setTransform 1 0 0 1 (+ fx (* x 20)) (+ fy (* y 20)))
+        (c/set-fill! (make-fill context))
+        (.fillRect -20 -20 40 40)))))
 
 (defn draw-line [context x y [c & r]]
   (draw-figure context x y (f/get-figure c))
@@ -27,6 +27,4 @@
     (draw-text context x (+ y 180) r)))
 
 (defn ^:export draw [x y text]
-  (let [c (c/get-context)]
-    (c/set-fill! c "#005000")
-    (draw-text c x y (s/split-lines text))))
+  (draw-text (c/get-context) x y (s/split-lines text)))
